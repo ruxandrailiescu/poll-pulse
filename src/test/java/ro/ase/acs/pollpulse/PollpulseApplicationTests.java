c0dd385938b9ddcd82863147d6d352d90f718a2e
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2025-03-12 12:06:55 +0200

    Initial commit

---
+++ src/test/java/ro/ase/acs/pollpulse/PollpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.ase.acs.pollpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
